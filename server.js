5e0183d06b1abf081824edd6294880babcbcc3f8
Roanan Platt <roanan.platt@gmail.com>
2022-06-26 18:05:52 -0600

    made table and added candidates

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+      // Your MySQL username,
+        user: 'root',
+      // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.use((req, res) => {
     res.status(404).end();
 });
